fa456335a0615f3cfd1e11686d8658308323f7fa
Saiful Islam <sakibbhaiya143@gmail.com>
2023-07-27 19:56:56 +0600

    Task 05 added

---
+++ task05.js
@@ -0,0 +1,14 @@
+const calculator =(num1,num2,operator)=>{
+    if(operator =='+'){
+        return num1+num2;
+    }else if(operator =='-'){
+        return num1-num2;
+    }else if(operator =='*'){
+        return num1*num2;
+    }else{
+        return num1/num2
+    }
+}
+
+const result = calculator(2,2,'/')
+console.log(result)
